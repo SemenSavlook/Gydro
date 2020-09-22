d29834e07353f72b54f65aa664582440a8e5ff27
SemenSavlook <semensavluk@gmail.com>
2020-09-22 22:29:10 +0600

    final version 22sep2020

--- js/main.js
+++ js/main.js
@@ -114,6 +114,15 @@ $(function () {
 		$(this).next().slideToggle();
 	});
 
-	
-
 });
+
+(function () {
+	var func = EventTarget.prototype.addEventListener;
+
+	EventTarget.prototype.addEventListener = function (type, fn, capture) {
+		this.func = func;
+		capture = capture || {};
+		capture.passive = false;
+		this.func(type, fn, capture);
+	};
+}());
